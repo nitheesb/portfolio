465dcf130b3c94a065491cb8dbcceedd077a4f4e
nitheesb <nitheesbalaji@gmail.com>
2025-11-24 11:22:53 +0700

    portfolio

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/about', (req, res) => res.render('about'));
+app.get('/services', (req, res) => res.render('services'));
+app.get('/contact', (req, res) => res.render('contact'));
+
+app.listen(3000, () => console.log('DevOps site live at http://localhost:3000'));
