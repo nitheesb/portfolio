d8eff259ef469a7309990d6228f07e8ef1bd2ce8
nitheesb <nitheesbalaji@gmail.com>
2025-11-24 16:21:03 +0700

    Updated by gemini

--- server.js
+++ server.js
@@ -10,5 +10,7 @@ app.get('/', (req, res) => res.render('index'));
 app.get('/about', (req, res) => res.render('about'));
 app.get('/services', (req, res) => res.render('services'));
 app.get('/contact', (req, res) => res.render('contact'));
+app.get('/projects', (req, res) => res.render('projects'));
+app.get('/testimonials', (req, res) => res.render('testimonials'))
 
 app.listen(3000, () => console.log('DevOps site live at http://localhost:3000'));
