7fb1a2281178596ec3b1037ed08602468123655c
nitheesb <nitheesbalaji@gmail.com>
2025-11-27 10:38:49 +0700

    Adding more content

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// FIX: Remove X-Powered-By header (Security/SEO requirement)
+app.disable('x-powered-by');
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.render('index'));
-app.get('/about', (req, res) => res.render('about'));
-app.get('/services', (req, res) => res.render('services'));
-app.get('/contact', (req, res) => res.render('contact'));
-app.get('/projects', (req, res) => res.render('projects'));
-app.get('/testimonials', (req, res) => res.render('testimonials'))
 
-app.listen(3000, () => console.log('DevOps site live at http://localhost:3000'));
+app.listen(3000, () => console.log('Portfolio running at http://localhost:3000'));
